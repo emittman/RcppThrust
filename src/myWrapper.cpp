dfa3a5d465939b9286e5ec25b55861c8e48edb76
emittman <emittman@iastate.edu>
2017-04-17 13:26:31 -0500

    Diagnose problem

--- src/myWrapper.cpp
+++ src/myWrapper.cpp
@@ -1,6 +1,8 @@
 #include <Rcpp.h>
 #include <vector>
+#include <iostream>
 #include <thrust/device_vector.h>
+#include <thrust/copy.h>
 #include "myHeader.h"
 using namespace Rcpp;
 
@@ -8,7 +10,10 @@ using namespace Rcpp;
 NumericVector myCppF(NumericVector Rx) {
   std::vector<double> x_h = Rcpp::as<std::vector<double> >(Rx);
   thrust::device_vector<double> x_d(x_h.begin(), x_h.end());
-  myOp(x_d);
+  myOp(x_d, verbose);
   thrust::copy(x_d.begin(), x_d.end(), x_h.begin());
+  for(int i=0; i<x_h.size(); i++)
+    std::cout << "\t" << x_h[i];
+  std::cout <<"\n";
   return Rx;
 }
